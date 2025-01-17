c6dbdeb410d11b0cace80a8a3087d8a8cf873e7d
Danilo Manchon <danilomanchon@gmail.com>
2025-01-16 23:10:40 -0300

    initial commit

---
+++ src/main/java/com/fiap/video_processor/sqs/SqsConsumer.java
@@ -0,0 +1,38 @@
+package com.fiap.video_processor.sqs;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class SqsConsumer {
+
+    @Value("${aws.queueName}")
+    private String queueName;
+
+    private final AmazonSQS amazonSQSClient;
+
+    public SqsConsumer(AmazonSQS amazonSQSClient) {
+        this.amazonSQSClient = amazonSQSClient;
+    }
+
+    public void consumeMessages() {
+        try {
+            String queueUrl = amazonSQSClient.getQueueUrl(queueName).getQueueUrl();
+
+            ReceiveMessageResult receiveMessageResult = amazonSQSClient.receiveMessage(queueUrl);
+
+            if (!receiveMessageResult.getMessages().isEmpty()) {
+                com.amazonaws.services.sqs.model.Message message = receiveMessageResult.getMessages().get(0);
+                log.info("Read Message from queue: {}", message.getBody());
+                amazonSQSClient.deleteMessage(queueUrl, message.getReceiptHandle());
+            }
+
+        } catch (Exception e) {
+            log.error("Queue Exception Message: {}", e.getMessage());
+        }
+    }
+}
