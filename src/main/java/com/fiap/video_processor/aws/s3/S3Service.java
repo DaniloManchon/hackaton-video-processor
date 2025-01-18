28f74c8b5cb1418ba92a5395b3b38024d2e267d9
Danilo Manchon <danilomanchon@gmail.com>
2025-01-18 12:22:55 -0300

    initial commit

--- src/main/java/com/fiap/video_processor/aws/s3/S3Service.java
+++ src/main/java/com/fiap/video_processor/aws/s3/S3Service.java
@@ -1,12 +1,13 @@
 package com.fiap.video_processor.aws.s3;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import software.amazon.awssdk.services.s3.S3Client;
 
 public class S3Service {
     private final S3Client s3Client;
     private final AwsProperties awsProperties;
 
-    public AmazonS3Service(@Qualifier("s3Client") S3Client s3Client, AwsProperties awsProperties) {
+    public S3Service(@Qualifier("s3Client") S3Client s3Client, AwsProperties awsProperties) {
         this.s3Client = s3Client;
         this.awsProperties = awsProperties;
     }
